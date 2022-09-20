0553ca26f8061e7dac5ba83e8a51e81b7aed061e
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-09-20 12:17:42 +0400

    Completed

--- index.js
+++ index.js
@@ -39,3 +39,47 @@ function Circle(radius) {
 const constructorCircle = new Circle(1);
 
 //---------------------------------------------------------
+
+// Creating a stopwatch.
+// Note: new JS changed it from a constructor function to a class declaration/
+class Stopwatch {
+    constructor() {
+        let startTime, endTime, duration, running = 0;
+        this.start = function () {
+            if (running) {
+                throw new Error('Stopwatch already running.');
+            }
+            startTime = new Date();
+            running = true;
+        };
+        this.stop = function () {
+            if (!running) {
+                throw new Error('Stopwatch is not running.');
+            }
+            running = false;
+            endTime = new Date();
+
+            const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+            duration = seconds;
+        };
+
+        this.reset = function () {
+            startTime = null;
+            endTime = null;
+            running = false;
+            duration = 0;
+        };
+
+        Object.defineProperty(this, 'duration', {
+            get: function() { return duration; }
+        });
+    }
+}
+
+// To test the Stopwatch function run the code below:
+
+// const sw = new Stopwatch();
+// sw.start();
+// sw.stop();
+// sw.duration;
+// sw.reset();
